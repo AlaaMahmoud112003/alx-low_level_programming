65b1d90604f62f7c0f54c0b8fbe21b712baf7e53
AlaaMahmoud112003 <full.stack.developer112003@gmail.com>
2023-07-31 14:24:45 -0700

    c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strstr - Entry  any point
- * @s: input the data
- * @accept: input any data
+ * _strstr - Entry any point
+ * @haystack: input data
+ * @needle: input any data
  * Return: Always 0 is (Successed)
  */
 char *_strstr(char *haystack, char *needle)
@@ -10,15 +10,15 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *x = haystack;
-		char *p = needle;
+		char *y = needle;
 
-		while (*x == *p && *p != '\0')
+		while (*x == *y && *y != '\0')
 		{
 			x++;
-			p++;
+			y++;
 		}
 
-		if (*p == '\0')
+		if (*y == '\0')
 			return (haystack);
 	}
 
