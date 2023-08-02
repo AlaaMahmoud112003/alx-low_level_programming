69b320d8e25ed4e632ffc5365b832f692174289f
AlaaMahmoud112003 <full.stack.developer112003@gmail.com>
2023-08-02 14:47:42 -0700

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,17 +10,15 @@ int actual_sqrt_recursion(int n, int x);
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return (-1);
-        return (actual_sqrt_recursion(n, 0));
+      if (n < 0)
+	     return (-1);
+	return (actual_sqrt_recursion(n, 0));
 }
-
 /**
  * actual_sqrt_recursion - recurse to find natural
  * square the root of number
  * @n: number to calculate sqaure root
  * @x: the iterator
- *
  * Return: result square root
  */
 int actual_sqrt_recursion(int n, int x)
