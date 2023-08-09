f7e97157189104bb400ffcc2043f5c4acb61e7b5
AlaaMahmoud112003 <full.stack.developer112003@gmail.com>
2023-08-08 18:19:55 -0700

    c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * alloc_grid - nested loop 
+ * alloc_grid - nested loop
  * @width: the input width
  * @height: the input height
  * Return: the pointer to array
