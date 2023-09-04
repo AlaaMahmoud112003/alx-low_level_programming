ff778950b70b895f452f25a4ae2709becea9fe25
AlaaMahmoud112003 <full.stack.developer112003@gmail.com>
2023-09-04 16:02:25 -0700

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int x;
-	unsigned int y;
+	int i;
+	unsigned int dec_val = 0;
 
-	y = 0;
 	if (!b)
 		return (0);
-	for (x = 0; b[x] != '\0'; x++)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[x] != '0' && b[x] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	for (x = 0; b[x] != '\0'; x++)
-	{
-		y <<= 1;
-		if (b[x] == '1')
-			y += 1;
-	}
-	return (y);
+
+	return (dec_val);
 }
